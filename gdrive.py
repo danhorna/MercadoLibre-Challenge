137231f605414eeb0b5057613e5e26c506e8689b
Dan Horna <dan_franco_15@hotmail.com>
2021-01-18 17:18:48 -0300

    initializing

---
+++ gdrive.py
@@ -0,0 +1,61 @@
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+class GoogleDrive():
+    def __init__(self):
+        # If modifying these scopes, delete the file token.pickle.
+        SCOPES = ['https://www.googleapis.com/auth/drive']
+        """Shows basic usage of the Drive v3 API.
+        Prints the names and ids of the first 10 files the user has access to.
+        """
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        self.service = build('drive', 'v3', credentials=creds)
+    
+    def all_files(self):
+        # Call the Drive v3 API
+        results = self.service.files().list(q="fullText contains 'test'",
+            pageSize=10, fields="nextPageToken, files(id, name, mimeType)").execute()
+        items = results.get('files', [])
+        if not items:
+            print('No files found.')
+        else:
+            print('Files:')
+            for item in items:
+                print(u'{0} ({1}) - {2}'.format(item['name'], item['id'], item['mimeType']))
+
+
+def main():
+    gDrive = GoogleDrive()
+    gDrive.all_files()
+    
+    # file_metadata = {
+    #     'name': 'pruebaz',
+    #     'description': 'probandingg'
+    # }
+    # file = service.files().create(body=file_metadata,
+    #                                     fields='id').execute()
+    # print('File ID: %s' % file.get('id'))
+
+if __name__ == '__main__':
+    main()
